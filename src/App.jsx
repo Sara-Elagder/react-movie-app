3b9bdc11d2e985125948945c3b2891bbaa7824b0
farah sharban <100386507+biof-farah@users.noreply.github.com>
2025-03-28 13:29:04 +0200

    Update App.jsx
    - remove excess testing components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import './App.css'
-import MovieList from './pages/MovieList'
 function App() {
 return (
-    <>
-    <h1 className='font-bold'>hello</h1>
-    <MovieList></MovieList>
-    </>
+    <>  </>
   )
 }
 export default App
