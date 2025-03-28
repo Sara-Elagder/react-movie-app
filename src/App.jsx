2c92752d05b210220bc48fefab0d59d6006471d2
biof-farah <farahsharban119@gmail.com>
2025-03-28 16:42:59 +0200

    routing intializing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+ import { BrowserRouter } from "react-router-dom"
+ import RoutesList from "./routes/RoutesList";
  function App(){
  return (
-    <> </>
+    <>
+    <BrowserRouter>
+  <RoutesList>
+    
+  </RoutesList>
+    </BrowserRouter>
+     </>
   )
  }
 export default App
