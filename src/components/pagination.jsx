7c6b14f3805051eea4cd38bb19029d76d025a2a4
PyMustafa <proo.eng2018@gmail.com>
2025-03-31 00:55:51 +0200

    fix(pagination): fix pagination component styling

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,30 +1,39 @@
-import React from "react"; 
+import React from "react";
+import "./pagination.css"; // We'll create this file shortly
 
-const Pagination =({currentPage, totalPages, onPageChange}) => {
-return(
-    <div className="pagination">
-            <button
-    className="pagination-btn"
-    onClick={() => onPageChange(currentPage -1)}
-    disabled={currentPage === 1}
-    >
-        &lsaquo;
-    </button>
-    <button
-    className="pagination-btn"
-    onClick={() => onPageChange(currentPage -1)}
-    disabled={currentPage === 1}
-    >
-        {currentPage-1}
-    </button>
-<button className="pagination-btn-active" onClick={() => onPageChange(currentPage)}> {currentPage} </button>
-<button
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  return (
+    <div className="pagination-container">
+      <button
         className="pagination-btn"
-        onClick={() => onPageChange(currentPage + 1)}
-        disabled={currentPage === totalPages}
+        onClick={() => onPageChange(currentPage - 1)}
+        disabled={currentPage === 1}
       >
-        {currentPage + 1}
+        &lsaquo;
       </button>
+
+      {currentPage > 1 && (
+        <button
+          className="pagination-btn"
+          onClick={() => onPageChange(currentPage - 1)}
+        >
+          {currentPage - 1}
+        </button>
+      )}
+
+      <button className="pagination-btn-active">
+        {currentPage}
+      </button>
+
+      {currentPage < totalPages && (
+        <button
+          className="pagination-btn"
+          onClick={() => onPageChange(currentPage + 1)}
+        >
+          {currentPage + 1}
+        </button>
+      )}
+
       <button
         className="pagination-btn"
         onClick={() => onPageChange(currentPage + 1)}
@@ -32,8 +41,8 @@ return(
       >
         &rsaquo;
       </button>
-</div>
-)
-}
+    </div>
+  );
+};
 
-export default Pagination
+export default Pagination;
